039c5c7abe13edd3e07a7b5afe4905f9cb985329
Malcolm Roberts <squidgyhead@gmail.com>
2014-03-17 11:31:32 +0100

    Tabify for Thomasify.

--- src/solid_solver/init_beam.f90
+++ src/solid_solver/init_beam.f90
@@ -11,26 +11,26 @@ subroutine init_beams ( beams )
 
   write (*,'("*** initializing ",i1," beams")')  nBeams
   write (*,'("--- Beam width (2*t_beam) covers ",(f4.1)," points")') 2.0*t_beam/max(dy,dx)
- 
+
   !-------------------------------------------
   ! allocate beam storage for each beam
   !-------------------------------------------
   do i = 1, nBeams
-    allocate ( beams(i)%x(0:ns-1) )
-    allocate ( beams(i)%y(0:ns-1) )
-    allocate ( beams(i)%vx(0:ns-1) )
-    allocate ( beams(i)%vy(0:ns-1) )
-    allocate ( beams(i)%ax(0:ns-1) )
-    allocate ( beams(i)%ay(0:ns-1) )
-    allocate ( beams(i)%theta(0:ns-1) )
-    allocate ( beams(i)%theta_dot(0:ns-1) )
-    allocate ( beams(i)%pressure_old(0:ns-1) )
-    allocate ( beams(i)%pressure_new(0:ns-1) )
-    allocate ( beams(i)%tau_old(0:ns-1) )
-    allocate ( beams(i)%tau_new(0:ns-1) )    
-    allocate ( beams(i)%beam_oldold(0:ns-1,1:6) )    
-  enddo 
-  
+     allocate ( beams(i)%x(0:ns-1) )
+     allocate ( beams(i)%y(0:ns-1) )
+     allocate ( beams(i)%vx(0:ns-1) )
+     allocate ( beams(i)%vy(0:ns-1) )
+     allocate ( beams(i)%ax(0:ns-1) )
+     allocate ( beams(i)%ay(0:ns-1) )
+     allocate ( beams(i)%theta(0:ns-1) )
+     allocate ( beams(i)%theta_dot(0:ns-1) )
+     allocate ( beams(i)%pressure_old(0:ns-1) )
+     allocate ( beams(i)%pressure_new(0:ns-1) )
+     allocate ( beams(i)%tau_old(0:ns-1) )
+     allocate ( beams(i)%tau_new(0:ns-1) )    
+     allocate ( beams(i)%beam_oldold(0:ns-1,1:6) )    
+  enddo
+
   !---------------------------------------------
   ! define adjustable parameters for each beam 
   ! this is position and motion protocoll
@@ -40,48 +40,48 @@ subroutine init_beams ( beams )
   beams(1)%iMouvement = iMotion
   beams(1)%AngleBeam = AngleBeam
   beams(1)%phase = 0.0  
-  
+
   !---------------------------------------------
   ! loop over beams and initialize them
   !---------------------------------------------
   do i = 1, nBeams
-    ! fetch leading edge position 
-    call mouvement(0.0, alpha, alpha_t, alpha_tt, LeadingEdge, beams(i) )
-    ! initialize as zero
-    beams(i)%x = 0.0
-    beams(i)%y = 0.0
-    beams(i)%vx = 0.0
-    beams(i)%vy = 0.0
-    beams(i)%ax = 0.0
-    beams(i)%ay = 0.0
-    beams(i)%theta = 0.0
-    beams(i)%theta_dot = 0.0
-    beams(i)%pressure_old = 0.0
-    beams(i)%pressure_new = 0.0
-    beams(i)%tau_old = 0.0
-    beams(i)%tau_new = 0.0   
-    beams(i)%beam_oldold = 0.0
-    beams(i)%Inertial_Force = 0.0
-    ! this is used for unst correction computation:
-    beams(i)%drag_unst_new = 0.0
-    beams(i)%drag_unst_old = 0.0
-    beams(i)%lift_unst_new = 0.0
-    beams(i)%lift_unst_old = 0.0
-    beams(i)%UnsteadyCorrectionsReady = .false.
-    beams(i)%StartupStep = .true.
-    beams(i)%dt_old = 0.0
-    
-    ! remember that what follows is described in relative coordinates 
-    do n = 0, ns-1 
-      beams(i)%x(n) = LeadingEdge(1) + real(n)*ds
-      beams(i)%y(n) = LeadingEdge(2)
-      beams(i)%vx(n)= 0.0
-      beams(i)%vy(n)= 0.0
-      beams(i)%ax(n)= 0.0
-      beams(i)%ay(n)= 0.0
-    enddo
+     ! fetch leading edge position 
+     call mouvement(0.0, alpha, alpha_t, alpha_tt, LeadingEdge, beams(i) )
+     ! initialize as zero
+     beams(i)%x = 0.0
+     beams(i)%y = 0.0
+     beams(i)%vx = 0.0
+     beams(i)%vy = 0.0
+     beams(i)%ax = 0.0
+     beams(i)%ay = 0.0
+     beams(i)%theta = 0.0
+     beams(i)%theta_dot = 0.0
+     beams(i)%pressure_old = 0.0
+     beams(i)%pressure_new = 0.0
+     beams(i)%tau_old = 0.0
+     beams(i)%tau_new = 0.0   
+     beams(i)%beam_oldold = 0.0
+     beams(i)%Inertial_Force = 0.0
+     ! this is used for unst correction computation:
+     beams(i)%drag_unst_new = 0.0
+     beams(i)%drag_unst_old = 0.0
+     beams(i)%lift_unst_new = 0.0
+     beams(i)%lift_unst_old = 0.0
+     beams(i)%UnsteadyCorrectionsReady = .false.
+     beams(i)%StartupStep = .true.
+     beams(i)%dt_old = 0.0
+
+     ! remember that what follows is described in relative coordinates 
+     do n = 0, ns-1 
+        beams(i)%x(n) = LeadingEdge(1) + real(n)*ds
+        beams(i)%y(n) = LeadingEdge(2)
+        beams(i)%vx(n)= 0.0
+        beams(i)%vy(n)= 0.0
+        beams(i)%ax(n)= 0.0
+        beams(i)%ay(n)= 0.0
+     enddo
      ! to take static angle into account
-    call integrate_position (0.0, beams(i))
+     call integrate_position (0.0, beams(i))
   enddo
-  
+
 end subroutine init_beams
