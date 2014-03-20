1081bda1d00d22f9aadb9ccfa66f5a5c8d9dc8e2
Thomas Engels <thomas.engels@mailbox.tu-berlin.de>
2014-03-20 21:03:36 +0100

    finalize mask generation for beam, including th e moving reference frame

--- src/geometry/hyd/draw_flexible_plate.f90
+++ src/geometry/hyd/draw_flexible_plate.f90
@@ -11,105 +11,135 @@ subroutine Draw_flexible_plate (time, beam)
 
   real(kind=pr), intent(in) :: time
   type(Solid), intent(in) :: beam
-  real(kind=pr) :: psi, gamma, R, tmp
-  real(kind=pr),dimension(1:3) :: x, x_plate, x0_plate
+  real(kind=pr) :: psi,gamma,tmp,tmp2,psi_dt,beta_dt,gamma_dt
+  real(kind=pr),dimension(1:3) :: x, x_plate, x0_plate,u_tmp,rot_body,v_tmp,v0_plate
   real(kind=pr),dimension(1:3,1:3) :: M_plate,M1,M2,M3
   !-- for the triangles:
-  real(kind=pr) :: a,b,c,alpha,beta,h
+  real(kind=pr) :: a,b,c,alpha,beta,h, safety, t1, s,s1,s2, ux,uy
   integer :: ix,iy,iz,is
   
-!   type(solid), dimension(1:nBeams) :: beams
-!   real (kind=pr) :: time2=0.d0
+  t1=MPI_wtime()
   
+  N_smooth = 3.d0  
+  safety = 2.d0*N_smooth*max(dx,dy,dz)
+    
+  !-- get relative coordinate system
+  call plate_coordinate_system( time,x0_plate,v0_plate,psi,beta,gamma,psi_dt,beta_dt,gamma_dt)
 
-  
-  !-- angles that rotate the plate system
-  psi = deg2rad(0.d0)
-  beta = deg2rad(0.d0)
-  gamma = deg2rad(0.d0)
-  
-  !-- displacement vetor
-!   x0_plate = (/ 0.5*xl,0.5*yl,0.5*zl /)
-  x0_plate = 0.d0
-!   R = 0.0
-!   x0_plate = (/ x0,y0+cos(psi)*R,z0+sin(psi)*R /)
-  
+  !-- rotation matrices that take us to the relative system
   call Rx(M1,psi)
   call Ry(M2,beta)
   call Rz(M3,gamma)
   M_plate = matmul(M1,matmul(M2,M3))
+  ! angular velocity of moving relative frame
+  rot_body = (/psi_dt, beta_dt, gamma_dt/)
   
-  mask = 5000.d0
   
-  
-!     !-- generate some deflection line
-!   x0=0.d0
-!   y0=0.d0
-!   call init_beams( beams )
-!   do while ((time2<=1.25))
-!     call SolidSolverWrapper( time2, dt_fixed , beams )
-!     time2 = time2+dt_fixed
-!   enddo
-!   
-!   beam = beams(1)
-  
+  mask = 0.d0
+  us = 0.d0
   
   !-- For all grid points of this subdomain
   do iz = ra(3), rb(3)
    do iy = ra(2), rb(2)
     do ix = ra(1), rb(1)
       !-- global coordinates
-      x = (/ dble(ix)*dx, dble(iy)*dy, dble(iz)*dz /)
+      x = (/ dble(ix)*dx, dble(iy)*dy, dble(iz)*dz/)
       !-- in the plate system
-      x_plate = matmul(M_plate,x-x0_plate)
+      x_plate = matmul( M_plate, x-x0_plate )
+      
+      
+      !-- check z-size in plate coordinate sytem (thus spanwise)
+      if ((x_plate(3)>=-(0.5*L_span+safety)).and.(x_plate(3)<=(0.5*L_span+safety))) then
+      !-- check x-size in plate system (so length, which is unity)
+      if ((x_plate(1)>=-(0.0+safety)).and.(x_plate(1)<=(1.0+safety))) then
+      !-- the beam bends in the x-y plane, so its maximum y-coordinate is -+1
+      !-- if its handing straight down or up
+      if ((x_plate(2)>=-(1.0+safety)).and.(x_plate(2)<=(1.0+safety))) then
+      
+      
+      !-- initialize mask (locally) as very far away and velocity as zero
+      mask(ix,iy,iz) = 5000.d0
+      ux = 0.d0
+      uy = 0.d0
       
       !-- loop over points on the beam
       do is = 0,ns-2
-        !-- a,b,c: sides of triangle
-        a = dsqrt( (x_plate(1)-beam%x(is))**2 + (x_plate(2)-beam%y(is))**2 )
-        !-- if the first lagrangian marker is too far away, the second
-        !-- one is as well, and we can skip the whole point, greatly
-        !-- reducing the computational complexity
-        if ( a < 4.d0*t_beam ) then
-          b = dsqrt( (x_plate(1)-beam%x(is+1))**2 + (x_plate(2)-beam%y(is+1))**2 )
-          !-- c is the distance between two markers, thus ds
-          c = dsqrt( (beam%x(is)-beam%x(is+1))**2 + (beam%y(is)-beam%y(is+1))**2 )
-          !-- angles in the triangle
-          alpha = acos ( (b**2+c**2-a**2)/(2.d0*b*c) )
-          beta  = acos ( (a**2+c**2-b**2)/(2.d0*a*c) )
-          
-          !-- height of the triangle: this is what we were looking for!
-          h = dsin(alpha)*b
-          
-          if ((abs(alpha)<= pi/2.0).and.(abs(beta)<=pi/2.d0)) then
-            !-- we're in the area of the line segment, where the
-            !-- height of the triangle defines the distance function
-            mask(ix,iy,iz) = min(h, mask(ix,iy,iz))
-          else
-            !-- we're in a hinge zone, where it is the closest
-            !-- lagrangian marker that defines the distance (circular hinges)
-            mask(ix,iy,iz) = min(mask(ix,iy,iz),min(a,b))
+          !-- a,b,c: sides of triangle
+          a = dsqrt( (x_plate(1)-beam%x(is))**2 + (x_plate(2)-beam%y(is))**2 )
+          !-- if the first lagrangian marker is too far away, the second
+          !-- one is as well, and we can skip the whole point, greatly
+          !-- reducing the computational complexity
+          if ( a < t_beam+safety ) then
+            b = dsqrt( (x_plate(1)-beam%x(is+1))**2 + (x_plate(2)-beam%y(is+1))**2 )
+            !-- c is the distance between two markers, thus ds
+            c = dsqrt( (beam%x(is)-beam%x(is+1))**2 + (beam%y(is)-beam%y(is+1))**2 )
+            !-- angles in the triangle (Law of cosines)
+            alpha = acos ( (b*b+c*c-a*a)/(2.d0*b*c) )
+            beta  = acos ( (a*a+c*c-b*b)/(2.d0*a*c) )
+            
+            !-- height of the triangle: this is what we were looking for!
+            h = dsin(alpha)*b
+            
+            
+            if ((abs(alpha)<=pi/2.d0).and.(abs(beta)<=pi/2.d0)) then
+                !-- we're in the area of the line segment, where the
+                !-- height of the triangle defines the distance function
+                if ( h<= mask(ix,iy,iz) ) then
+                  mask(ix,iy,iz) = h
+                
+                  !-- linear interpolation of velocity
+                  s1 = dble(is)*ds
+                  s2 = dble(is+1)*ds
+                  s = s1 + dsqrt(b*b - h*h)
+                  ux = beam%vx(is) + ((s-s1)/(s2-s1))*(beam%vx(is+1)-beam%vx(is))
+                  uy = beam%vy(is) + ((s-s1)/(s2-s1))*(beam%vy(is+1)-beam%vy(is))
+                endif
+            else
+                !-- we're in a hinge zone, where it is the closest
+                !-- lagrangian marker that defines the distance (circular hinges)
+                mask(ix,iy,iz) = min(mask(ix,iy,iz),min(a,b))
+                
+                if (a==mask(ix,iy,iz)) then
+                  ux = beam%vx(is)
+                  uy = beam%vy(is)
+                elseif (b==mask(ix,iy,iz) ) then
+                  ux = beam%vx(is+1)
+                  uy = beam%vy(is+1)
+                endif
+                
+            endif          
           endif
-          
-        endif
       enddo
+  
+      !-- mask is now the distance function from the centerline
+      !-- convert distance function to mask function
+      call smoothstep( tmp, mask(ix,iy,iz)-t_beam, 0.d0, N_smooth*max(dx,dy,dz) )
+      !-- make plate finite in z-direction
+      call smoothstep( tmp2, abs(x_plate(3)), L_span, N_smooth*max(dx,dy,dz) )
+      !-- final value
+      mask(ix,iy,iz) = tmp*tmp2
       
       
+      !-- this is the velocity in the relative system
+      u_tmp = (/ux,uy,0.d0/)
+      !-- add solid body rotation to the velocity field of the beam
+      v_tmp(1) = v0_plate(1) + rot_body(2)*x_plate(3)-rot_body(3)*x_plate(2)
+      v_tmp(2) = v0_plate(2) + rot_body(3)*x_plate(1)-rot_body(1)*x_plate(3)
+      v_tmp(3) = v0_plate(3) + rot_body(1)*x_plate(2)-rot_body(2)*x_plate(1)
+      ! up to now, all velocities are in the relative system, now bring them back
+      us(ix,iy,iz,1:3)=matmul(transpose(M_plate),u_tmp+v_tmp)
+      
+      endif !-- end of bounding box checks
+      endif
+      endif
     enddo
    enddo
   enddo
+
+  write(*,*) "warning internal division by eps"
+  mask=mask/eps
   
-!   where (mask>1000.d0) mask=0.d0
-  
-  !-- at the end of thsi process, mask is the distance function from the centerline
-  do iz = ra(3), rb(3)
-   do iy = ra(2), rb(2)
-    do ix = ra(1), rb(1)  
-      call smoothstep( tmp, mask(ix,iy,iz)-t_beam, 0.d0, 3.d0*dx )
-      mask(ix,iy,iz) = tmp
-    enddo
-   enddo
-  enddo  
-  
-  !-- Calculate unsteady 
+  write(*,'("elapsed=",es12.4)') MPI_wtime()-t1
 end subroutine Draw_flexible_plate
+
+
