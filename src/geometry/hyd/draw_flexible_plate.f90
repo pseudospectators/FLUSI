38a79f53353712a61a7630ea39b6ca16aa75e222
Thomas Engels <thomas.engels@mailbox.tu-berlin.de>
2014-04-23 18:30:32 +0200

    bugfix and simplification of flexible mask generation

--- src/geometry/hyd/draw_flexible_plate.f90
+++ src/geometry/hyd/draw_flexible_plate.f90
@@ -15,13 +15,14 @@ subroutine Draw_flexible_plate (time, beam)
   real(kind=pr),dimension(1:3) :: x, x_plate, x0_plate,u_tmp,rot_body,v_tmp,v0_plate
   real(kind=pr),dimension(1:3,1:3) :: M_plate
   !-- for the triangles:
-  real(kind=pr) :: a,b,c,alpha,beta,h,safety, s,s1,s2, ux,uy, R  
+  real(kind=pr) :: a,b,c,alpha,beta,h,safety, s,s1,s2, ux,uy, R ,dmax, love
+  real(kind=pr) :: c1,c2
   !-- for leading edge state:
   real(kind=pr) :: alpha_t, alpha_tt 
   real(kind=pr), dimension(1:6) :: LeadingEdge
   integer :: ix,iy,iz,is
   
-  safety = 4.d0*N_smooth*max(dx,dy,dz,ds)
+  safety = 2.d0*N_smooth*max(dx,dy,dz)
     
   !-- get relative coordinate system
   call plate_coordinate_system( time,x0_plate,v0_plate,psi,beta,&
@@ -33,6 +34,9 @@ subroutine Draw_flexible_plate (time, beam)
   mask = 0.d0
   us = 0.d0
   
+  ! largest admissible sides (a,b) for triangles
+  dmax = dsqrt( (ds)**2 + (t_beam+safety)**2 )
+
   !-- For all grid points of this subdomain
   do iz = ra(3), rb(3)
    do iy = ra(2), rb(2)
@@ -44,12 +48,12 @@ subroutine Draw_flexible_plate (time, beam)
       
       
       !-- check z-size in plate coordinate sytem (thus spanwise)
-      if ((x_plate(3)>=-(0.5*L_span+safety)).and.(x_plate(3)<=(0.5*L_span+safety))) then
+      if ((x_plate(3)>=-(0.5*L_span+t_beam+safety)).and.(x_plate(3)<=(0.5*L_span+t_beam+safety))) then
       !-- check x-size in plate system (so length, which is unity)
-      if ((x_plate(1)>=-(0.0+safety)).and.(x_plate(1)<=(1.0+safety))) then
+      if ((x_plate(1)>=-(0.0+t_beam+safety)).and.(x_plate(1)<=(1.0+t_beam+safety))) then
       !-- the beam bends in the x-y plane, so its maximum y-coordinate is -+1
       !-- if its handing straight down or up
-      if ((x_plate(2)>=-(1.0+safety)).and.(x_plate(2)<=(1.0+safety))) then
+      if ((x_plate(2)>=-(1.0+t_beam+safety)).and.(x_plate(2)<=(1.0+t_beam+safety))) then
       
       
       !-- initialize mask (locally) as very far away and velocity as zero
@@ -59,56 +63,59 @@ subroutine Draw_flexible_plate (time, beam)
       
       !-- loop over points on the beam
       do is = 0,ns-2
-          !-- a,b,c: sides of triangle
-          a = dsqrt( (x_plate(1)-beam%x(is))**2 + (x_plate(2)-beam%y(is))**2 )
-          !-- if the first lagrangian marker is too far away, the second
-          !-- one is as well, and we can skip the whole point, greatly
-          !-- reducing the computational complexity
-          if ( a < t_beam+safety ) then
-            b = dsqrt( (x_plate(1)-beam%x(is+1))**2 + (x_plate(2)-beam%y(is+1))**2 )
-            !-- c is the distance between two markers, thus ds
-            c = dsqrt( (beam%x(is)-beam%x(is+1))**2 + (beam%y(is)-beam%y(is+1))**2 )
-            !-- angles in the triangle (Law of cosines)
-            tmp  = (b*b+c*c-a*a)/(2.d0*b*c)
-            tmp2 = (a*a+c*c-b*b)/(2.d0*a*c)            
-            alpha = acos ( tmp  )
-            beta  = acos ( tmp2 )
-            ! catch exceptions
-            if ((tmp<-1.d0).or.(tmp>1.d0)) alpha=0.d0
-            if ((tmp2<-1.d0).or.(tmp2>1.d0)) beta=0.d0
+        !-- a,b,c: sides of triangle
+        a = dsqrt( (x_plate(1)-beam%x(is))**2 + (x_plate(2)-beam%y(is))**2 )
+        b = dsqrt( (x_plate(1)-beam%x(is+1))**2 + (x_plate(2)-beam%y(is+1))**2 )
+        
+        ! at least one marker has to be close enough, otherwise we skip.
+        if ((a<=dmax).or.(b<=dmax)) then          
+          !-- c is the distance between two markers, thus ds
+          c = dsqrt( (beam%x(is)-beam%x(is+1))**2 + (beam%y(is)-beam%y(is+1))**2 )                    
+          !-- height of the triangle: this is what we were looking for! (distance)
+          h = dsqrt( a**2 - ((a**2+c**2-b**2)/(2.d0*c))**2)          
+          ! two parts on base side (i.e. on the centerline)
+          c1 = dsqrt( a**2-h**2)
+          c2 = dsqrt( b**2-h**2)
+          
+          !-- are both sides a,b small enough for the point to lie within the beam?
+          if ((a<=dmax).and.(b<=dmax).and.((c1<=c).and.(c2<=c))) then          
+            !-----------------------------------------------------------------
+            !-- we're in the area of the line segment, where the
+            !-- height of the triangle defines the distance function
+            !-----------------------------------------------------------------
+            if ( h <= mask(ix,iy,iz) ) then
+              mask(ix,iy,iz) = h
             
-            !-- height of the triangle: this is what we were looking for!
-            h = dsin(alpha)*b
+              !-- linear interpolation of velocity (along the element)
+              s1 = dble(is)*ds
+              s2 = dble(is+1)*ds
+              s = s1 + dsqrt(b*b - h*h)
+              ux = beam%vx(is+1) + ((s-s1)/(s2-s1))*(beam%vx(is)-beam%vx(is+1))
+              uy = beam%vy(is+1) + ((s-s1)/(s2-s1))*(beam%vy(is)-beam%vy(is+1))
+            endif
             
-            
-            if ((abs(alpha)<=pi/2.d0).and.(abs(beta)<=pi/2.d0)) then
-                !-- we're in the area of the line segment, where the
-                !-- height of the triangle defines the distance function
-                if ( h<= mask(ix,iy,iz) ) then
-                  mask(ix,iy,iz) = h
-                
-                  !-- linear interpolation of velocity (along the element)
-                  s1 = dble(is)*ds
-                  s2 = dble(is+1)*ds
-                  s = s1 + dsqrt(b*b - h*h)
-                  ux = beam%vx(is+1) + ((s-s1)/(s2-s1))*(beam%vx(is)-beam%vx(is+1))
-                  uy = beam%vy(is+1) + ((s-s1)/(s2-s1))*(beam%vy(is)-beam%vy(is+1))
-                endif
-            else
-                !-- we're in a hinge zone, where it is the closest
-                !-- lagrangian marker that defines the distance (circular hinges)
-                mask(ix,iy,iz) = min(mask(ix,iy,iz),min(a,b))
-                !-- assign the velocity of the closer hinge (if any)
-                if (a==mask(ix,iy,iz)) then
-                  ux = beam%vx(is)
-                  uy = beam%vy(is)
-                elseif (b==mask(ix,iy,iz) ) then
-                  ux = beam%vx(is+1)
-                  uy = beam%vy(is+1)
-                endif
-                
-            endif          
+          else            
+            !-----------------------------------------------------------------
+            !-- not on the segment, but maybe in a hinge? 
+            !-- which is the closer hinge?
+            !-----------------------------------------------------------------
+            if (a<b) then
+              !-- we're in the region of the LEFT hinge
+              if ( a <= mask(ix,iy,iz) ) then
+                mask(ix,iy,iz) = a
+                ux = beam%vx(is)
+                uy = beam%vy(is)
+              endif
+            elseif (b<a) then
+              !-- we're in the region of the RIGHt hinge
+              if ( b <= mask(ix,iy,iz) ) then
+                mask(ix,iy,iz) = b
+                ux = beam%vx(is+1)
+                uy = beam%vy(is+1)
+              endif
+            endif
           endif
+        endif
       enddo
   
       !-- mask is now the distance function from the centerline
