b4f59cc96e15af6b24b5d15383ed28b20c632c07
Dmitry Kolomenskiy dkolom@gmail.com <dkolom@login.cluster>
2014-06-10 21:10:29 +0200

    ramamurti flapper wing kinematics

--- src/insects/wings_motion.f90
+++ src/insects/wings_motion.f90
@@ -60,7 +60,7 @@ subroutine FlappingMotion(time, protocoll, phi, alpha, theta, phi_dt, alpha_dt, 
   real(kind=pr) :: ai_alpha(1:10), bi_alpha(1:10)
   real(kind=pr) :: bi_alpha_flapper(1:29) ! For comparison with Sane&Dickinson
   real(kind=pr) :: ai_phi_flapper(1:31) ! For comparison with Sane&Dickinson
-  real(kind=pr) :: tadv ! For comparison with Dickinson
+  real(kind=pr) :: tadv,t0adv ! For comparison with Dickinson, Ramamurti
   real(kind=pr) :: posi,elev,feth,posi_dt,elev_dt,feth_dt,angles ! Comp. w. Maeda
   real(kind=pr) :: dangle_posi,dangle_elev,dangle_feth ! Comp. w. Maeda
   real(kind=pr) :: dangle_posi_dt,dangle_elev_dt,dangle_feth_dt ! Comp. w. Maeda
@@ -415,6 +415,116 @@ subroutine FlappingMotion(time, protocoll, phi, alpha, theta, phi_dt, alpha_dt, 
     phi = phicdeg/180.0d0 * phi
     phi_dt = phicdeg/180.0d0 * phi_dt
    
+    ! convert in radians 
+    phi = deg2rad(phi)
+    phi_dt = deg2rad(phi_dt)
+
+    ! *** III. elevation ***
+    theta = 0.0d0
+    theta_dt = 0.0d0
+
+    !if(mpirank == 0) then
+    !open(14,file='motion.t',status='unknown',position='append')
+    !write (14,'(7(e12.5,1x))') time,phi,alpha,theta,phi_dt,alpha_dt,theta_dt
+    !close(14)
+    !endif
+
+  case ("flapper_ramamurti")
+    !---------------------------------------------------------------------------
+    ! motion protocol from Ramamurti and Sandberg, JEB (2002)
+    !
+    ! feathering: fourier coefficients analyzed with matlab,
+    !             Gaussian filter that fits fig 2
+    ! positional: similar to above
+    ! elevation:  zero
+    !
+    ! Dmitry, 10 Jun 2013
+    !---------------------------------------------------------------------------
+
+    ! Time shift as in JEB
+    t0adv = 0.28175d0
+
+    ! *** I. feathering motion ***
+    ! Note that this is feathering angle measured from the vertical.
+    ! This is NOT angle of attack
+!    bi_alpha_flapper =(/48.23094285611071d0,&
+!      0.0d0,10.224154661301371d0,0.0d0,2.1623763046726396d0,&
+!      0.0d0,0.05049394424178093d0,0.0d0,-0.17550942623071494d0,&
+!      0.0d0,-0.06634193748204852d0,0.0d0,-0.008925020495896451d0,&
+!      0.0d0,0.0011292567942149407d0,0.0d0,6.471071566666472d-4,&
+!      0.0d0,1.0018757795834964d-4,0.0d0,3.0105550216312524d-6,&
+!      0.0d0,-1.237567150768195d-6,0.0d0,-1.988004402010933d-7,&
+!      0.0d0,-1.10165545174181d-8,0.0d0,2.4135650975460306d-10/)
+
+    bi_alpha_flapper =(/58.5622945117485d0,&
+      0.0d0,9.70856389020196d0,0.0d0,1.06772463979698d0,&
+      0.0d0,-0.127455709998572d0,0.0d0,-0.0553559839123380d0,&
+      0.0d0,-0.00500430568361157d0,0.0d0,0.000181637429802491d0,&
+      0.0d0,5.24470001944736d-05,0.0d0,2.39488192125763d-06,&
+      0.0d0,-1.62462115890220d-08,0.0d0,-3.60732923972996d-09,&
+      0.0d0,-7.58943924434529d-11,0.0d0,-3.17275659750908d-16,&
+      0.0d0,1.50381167957580d-14,0.0d0,1.41146073296091d-16/)
+
+    ! Advanced rotation (+ sign) or delayed rotation (- sign)
+    tadv = 0.08d0
+    !tadv = - 0.08d0
+
+    alpha = 0.0d0
+    alpha_dt = 0.0d0
+    
+    ! frequency factor
+    f = 2.d0*pi
+    
+    ! Fourier series
+    do i=1,29
+      ! allows the spaces I like with the 80 columns malcolm likes :)
+      s = dsin(f*dble(i)*(time+tadv+t0adv)) 
+      c = dcos(f*dble(i)*(time+tadv+t0adv))
+      alpha = alpha + bi_alpha_flapper(i) * s
+      alpha_dt = alpha_dt + f*dble(i)* bi_alpha_flapper(i) * c
+    enddo
+
+    ! convert in radians 
+    alpha = deg2rad(alpha)
+    alpha_dt = deg2rad(alpha_dt)
+    
+    ! *** II. position ***
+!    ai_phi_flapper =(/63.24528806534019d0,&
+!      0.0d0,5.753991800610726d0,0.0d0,1.3887974015525626d0,&
+!      0.0d0,0.3889856512386744d0,0.0d0,0.10577402496901325d0,&
+!      0.0d0,0.026061339604144987d0,0.0d0,0.005623376646981709d0,&
+!      0.0d0,0.001042285996467963d0,0.0d0,1.639611509380189d-4,&
+!      0.0d0,2.1716252827442023d-5,0.0d0,2.408190194815521d-6,&
+!      0.0d0,2.2268710288534648d-7,0.0d0,1.7118916093759426d-8,&
+!      0.0d0,1.0914870312823793d-9,0.0d0,5.76135101855556d-11,&
+!      0.0d0,2.513944479978149d-12/)
+
+    a0_phi = 20.0d0
+    ai_phi_flapper =(/71.1023524748246d0,&
+      0.0d0,6.43355277659369d0,0.0d0,1.53592591540135d0,&
+      0.0d0,0.423188982190934d0,0.0d0,0.112580047175950d0,&
+      0.0d0,0.0269873997935609d0,0.0d0,0.00563410946130459d0,&
+      0.0d0,0.00100469248408058d0,0.0d0,0.000151188714225520d0,&
+      0.0d0,1.90437108218227d-05,0.0d0,1.99627349027768d-06,&
+      0.0d0,1.73399283714958d-07,0.0d0,1.24381425530736d-08,&
+      0.0d0,7.34711932879597d-10,0.0d0,3.56493134534579d-11,&
+      0.0d0,1.41757814786810d-12/)
+
+    phi = 0.5d0 * a0_phi
+    phi_dt = 0.0d0
+    
+    ! frequency factor
+    f = 2.d0*pi
+    
+    ! Fourier series
+    do i=1,31
+      ! allows the spaces I like with the 80 columns malcolm likes :)
+      s = dsin(f*dble(i)*(time+t0adv)) 
+      c = dcos(f*dble(i)*(time+t0adv))
+      phi   = phi   + ai_phi_flapper(i) * c
+      phi_dt   = phi_dt   + f*dble(i)*(-ai_phi_flapper(i) * s)
+    enddo
+    
     ! convert in radians 
     phi = deg2rad(phi)
     phi_dt = deg2rad(phi_dt)
