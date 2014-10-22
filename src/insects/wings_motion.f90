de4a31b2c90c76e7f6fe297afbb878d215ca97c3
Dmitry Kolomenskiy dkolom@gmail.com <dkolom@login.cluster>
2014-10-22 14:20:10 +0200

    Suzuki tst case kinematics

--- src/insects/wings_motion.f90
+++ src/insects/wings_motion.f90
@@ -730,7 +730,33 @@ subroutine FlappingMotion(time, Insect, protocoll, phi, alpha, theta, phi_dt, al
     ! *** III. elevation ***
     theta = 0.0d0
     theta_dt = 0.0d0
+
+  case ("suzuki")
+
+    ! frequency
+    f = 2.d0*pi
+
+    ! amplitudes and other parameters
+    phi_max = 80
+    alpha_max = 45
+    c = 3.3
+
+    ! *** I. position ***
+    phi = phi_max * cos(f*time)
+    phi_dt = -phi_max*f * sin(f*time)
+    phi = deg2rad(phi)
+    phi_dt = deg2rad(phi_dt)
+
+    ! *** II. feathering ***
+    alpha = alpha_max/tanh(c)*tanh(c*sin(f*time))
+    alpha_dt = alpha_max/tanh(c)*f*c*cos(f*time)*(1-tanh(c*sin(f*time))**2)
+    alpha = deg2rad(alpha)
+    alpha_dt = deg2rad(alpha_dt)
    
+    ! *** III. elevation ***
+    theta = 0.0d0
+    theta_dt = 0.0d0
+
   case ("takeoff")
     !--------------------------------------------------
     ! Fontaine et al. 
