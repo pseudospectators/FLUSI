77b9b7455fca25f41594891e6e5da384e7583236
Thomas Engels <thomas.engels@mailbox.tu-berlin.de>
2015-07-29 14:22:41 +0200

    improve vorticity and velocity postprocessing tools significantly

--- src/postprocessing.f90
+++ src/postprocessing.f90
@@ -424,7 +424,17 @@ end subroutine convert_abs_vorticity
 
 
 !-------------------------------------------------------------------------------
-! ./flusi --postprocess --vorticity ux_00000.h5 uy_00000.h5 uz_00000.h5 --second-order
+! Read velocity components from file and compute their curl. Optionally second order
+!
+! You can write to standard output: (vorx_0000.h5 in the example:)
+! ./flusi -p --vorticity ux_00000.h5 uy_00000.h5 uz_00000.h5 [--second-order]
+!
+! Or specify a prefix for the output files: (writes to curl_0000.h5 in example:)
+! ./flusi -p --vorticity ux_00000.h5 uy_00000.h5 uz_00000.h5 --outputprefix curl [--second-order]
+!
+! Or specifiy the ouput files directly:
+! ./flusi -p --vorticity ux_00000.h5 uy_00000.h5 uz_00000.h5 --outfiles vx_00.h5 vy_00.h5 vz_00.h5 [--second-order]
+!
 !-------------------------------------------------------------------------------
 ! load the velocity components from file and compute & save the vorticity
 ! can be done in parallel. the flag --second order can be used for filtering
@@ -434,7 +444,8 @@ subroutine convert_vorticity()
   use basic_operators
   use mpi
   implicit none
-  character(len=strlen) :: fname_ux, fname_uy, fname_uz, dsetname, order
+  character(len=strlen) :: fname_ux, fname_uy, fname_uz, order
+  character(len=strlen) :: prefix, fname_outx, fname_outy, fname_outz
   complex(kind=pr),dimension(:,:,:,:),allocatable :: uk
   real(kind=pr),dimension(:,:,:,:),allocatable :: u
   real(kind=pr) :: time
@@ -444,18 +455,51 @@ subroutine convert_vorticity()
   call get_command_argument(5,fname_uz)
   call get_command_argument(6,order)
 
+  if (order == "--outputprefix") then
+    call get_command_argument(7,prefix)
+    call get_command_argument(8,order)
+    fname_outx=trim(adjustl(prefix))//"x"//fname_ux(index(fname_ux,'_'):index(fname_ux,'.')-1)
+    fname_outy=trim(adjustl(prefix))//"y"//fname_uy(index(fname_uy,'_'):index(fname_uy,'.')-1)
+    fname_outz=trim(adjustl(prefix))//"z"//fname_uz(index(fname_uz,'_'):index(fname_uz,'.')-1)
+
+  elseif (order == "--outfiles") then
+    call get_command_argument(7,fname_outx)
+    call get_command_argument(8,fname_outy)
+    call get_command_argument(9,fname_outz)
+    call get_command_argument(10,order)
+
+  else
+    fname_outx='vorx'//fname_ux(index(fname_ux,'_'):index(fname_ux,'.')-1)
+    fname_outy='vory'//fname_uy(index(fname_uy,'_'):index(fname_uy,'.')-1)
+    fname_outz='vorz'//fname_uz(index(fname_uz,'_'):index(fname_uz,'.')-1)
+  endif
+
+  ! header and information
+  if (mpirank==0) then
+    write(*,'(80("-"))')
+    write(*,*) "vor2u (Biot-Savart)"
+    write(*,*) "Computing vorticity from velocity given in these files: "
+    write(*,'(80("-"))')
+    write(*,*) trim(adjustl(fname_ux))
+    write(*,*) trim(adjustl(fname_uy))
+    write(*,*) trim(adjustl(fname_uz))
+    write(*,*) "Writing to:"
+    write(*,*) trim(adjustl(fname_outx))
+    write(*,*) trim(adjustl(fname_outy))
+    write(*,*) trim(adjustl(fname_outz))
+    write(*,*) "Using order flag: "//trim(adjustl(order))
+    write(*,'(80("-"))')
+    if ((fname_ux(1:2).ne."ux").or.(fname_uy(1:2).ne."uy").or.(fname_uz(1:2).ne."uz")) then
+      write (*,*) "WARNING! in arguments, files do not start with ux uy and uz"
+      write (*,*) "note files have to be in the right order"
+    endif
+  endif
+
   call check_file_exists( fname_ux )
   call check_file_exists( fname_uy )
   call check_file_exists( fname_uz )
 
-  if ((fname_ux(1:2).ne."ux").or.(fname_uy(1:2).ne."uy").or.(fname_uz(1:2).ne."uz")) then
-    write (*,*) "Error in arguments, files do not start with ux uy and uz"
-    write (*,*) "note files have to be in the right order"
-    call abort()
-  endif
-
-  dsetname = fname_ux ( 1:index( fname_ux, '_' )-1 )
-  call fetch_attributes( fname_ux, dsetname, nx, ny, nz, xl, yl, zl, time )
+  call fetch_attributes( fname_ux, get_dsetname(fname_ux), nx, ny, nz, xl, yl, zl, time )
 
   pi=4.d0 *datan(1.d0)
   scalex=2.d0*pi/xl
@@ -474,9 +518,7 @@ subroutine convert_vorticity()
   call read_single_file ( fname_uy, u(:,:,:,2) )
   call read_single_file ( fname_uz, u(:,:,:,3) )
 
-  call fft (uk(:,:,:,1),u(:,:,:,1))
-  call fft (uk(:,:,:,2),u(:,:,:,2))
-  call fft (uk(:,:,:,3),u(:,:,:,3))
+  call fft3 (inx=u, outk=uk)
 
   if (order=="--second-order") then
     call curl_2nd(uk(:,:,:,1),uk(:,:,:,2),uk(:,:,:,3))
@@ -484,24 +526,12 @@ subroutine convert_vorticity()
     call curl(uk(:,:,:,1),uk(:,:,:,2),uk(:,:,:,3))
   endif
 
-  call ifft (u(:,:,:,1),uk(:,:,:,1))
-  call ifft (u(:,:,:,2),uk(:,:,:,2))
-  call ifft (u(:,:,:,3),uk(:,:,:,3))
+  call ifft3 (ink=uk, outx=u)
 
   ! now u contains the vorticity in physical space
-  fname_ux='vorx'//fname_ux(index(fname_ux,'_'):index(fname_ux,'.')-1)
-  fname_uy='vory'//fname_uy(index(fname_uy,'_'):index(fname_uy,'.')-1)
-  fname_uz='vorz'//fname_uz(index(fname_uz,'_'):index(fname_uz,'.')-1)
-
-
-  call save_field_hdf5 ( time,fname_ux,u(:,:,:,1))
-  if (mpirank==0) write(*,*) "Wrote vorx to "//trim(fname_ux)
-  call save_field_hdf5 ( time,fname_uy,u(:,:,:,2))
-  if (mpirank==0) write(*,*) "Wrote vory to "//trim(fname_uy)
-  call save_field_hdf5 ( time,fname_uz,u(:,:,:,3))
-  if (mpirank==0) write(*,*) "Wrote vorz to "//trim(fname_uz)
-
-
+  call save_field_hdf5 ( time,fname_outx,u(:,:,:,1) )
+  call save_field_hdf5 ( time,fname_outy,u(:,:,:,2) )
+  call save_field_hdf5 ( time,fname_outz,u(:,:,:,3) )
 
   deallocate (u)
   deallocate (uk)
@@ -512,35 +542,81 @@ end subroutine convert_vorticity
 
 
 !-------------------------------------------------------------------------------
-! ./flusi --postprocess --vor2u vorx_00000.h5 vory_00000.h5 vorz_00000.h5
+! Read in vorticity fields and compute the corresponding velocity (Biot-Savart Law)
+! Some checks are performed on the result, namely divergence(u) and we see
+! if the curl(result) is the same as the input values.
+!
+! you can optionally specify the list of outfiles:
+! ./flusi -p --vor2u vorx_00.h5 vory_00.h5 vorz_00.h5 --outfiles outx_00.h5 outy_00.h5 outz_00.h5
+!
+! or write to the default file names: (ux_00.h5 in the example)
+! ./flusi -p --vor2u vorx_00.h5 vory_00.h5 vorz_00.h5
+!
+! or specify the basename prefix: (writes to outx_00.h5 in the example:)
+! ./flusi -p --vor2u vorx_00.h5 vory_00.h5 vorz_00.h5 --outputprefix out
+!
 !-------------------------------------------------------------------------------
 subroutine convert_velocity()
   use vars
   use p3dfft_wrapper
   use basic_operators
+  use helpers
   use mpi
   implicit none
-  character(len=strlen) :: fname_ux, fname_uy, fname_uz, dsetname, order
+  character(len=strlen) :: fname_ux, fname_uy, fname_uz, outfiles_given
+  character(len=strlen) :: fname_outx, fname_outy, fname_outz, prefix
   complex(kind=pr),dimension(:,:,:,:),allocatable :: uk, workc
   real(kind=pr),dimension(:,:,:,:),allocatable :: u,workr
-  real(kind=pr) :: time
+  real(kind=pr) :: time, divu_max, errx, erry, errz
+  integer :: ix,iy,iz, mpicode
 
   call get_command_argument(3,fname_ux)
   call get_command_argument(4,fname_uy)
   call get_command_argument(5,fname_uz)
+  call get_command_argument(6,outfiles_given)
+
+  if ( outfiles_given == "--outfiles" ) then
+    call get_command_argument(7,fname_outx)
+    call get_command_argument(8,fname_outy)
+    call get_command_argument(9,fname_outz)
+  elseif ( outfiles_given == "--outputprefix" ) then
+    ! if not specified, use standard output names:
+    call get_command_argument(7,prefix)
+    fname_outx=trim(adjustl(prefix))//"x"//fname_ux(index(fname_ux,'_'):index(fname_ux,'.')-1)
+    fname_outy=trim(adjustl(prefix))//"y"//fname_uy(index(fname_uy,'_'):index(fname_uy,'.')-1)
+    fname_outz=trim(adjustl(prefix))//"z"//fname_uz(index(fname_uz,'_'):index(fname_uz,'.')-1)
+  else
+    ! if not specified, use standard output names:
+    fname_outx='ux'//fname_ux(index(fname_ux,'_'):index(fname_ux,'.')-1)
+    fname_outy='uy'//fname_uy(index(fname_uy,'_'):index(fname_uy,'.')-1)
+    fname_outz='uz'//fname_uz(index(fname_uz,'_'):index(fname_uz,'.')-1)
+  endif
+
+  ! header and information
+  if (mpirank==0) then
+    write(*,'(80("-"))')
+    write(*,*) "vor2u (Biot-Savart)"
+    write(*,*) "Computing velocity from vorticity given in these files: "
+    write(*,'(80("-"))')
+    write(*,*) trim(adjustl(fname_ux))
+    write(*,*) trim(adjustl(fname_uy))
+    write(*,*) trim(adjustl(fname_uz))
+    write(*,*) "Writing to:"
+    write(*,*) trim(adjustl(fname_outx))
+    write(*,*) trim(adjustl(fname_outy))
+    write(*,*) trim(adjustl(fname_outz))
+    write(*,'(80("-"))')
+    if ((fname_ux(1:4).ne."vorx").or.(fname_uy(1:4).ne."vory").or.(fname_uz(1:4).ne."vorz")) then
+      write (*,*) "WARNING in arguments, files do not start with vorx vory and vorz"
+      write (*,*) "note files have to be in the right order"
+    endif
+  endif
 
   call check_file_exists( fname_ux )
   call check_file_exists( fname_uy )
   call check_file_exists( fname_uz )
 
-  if ((fname_ux(1:4).ne."vorx").or.(fname_uy(1:4).ne."vory").or.(fname_uz(1:4).ne."vorz")) then
-    write (*,*) "Error in arguments, files do not start with vorx vory and vorz"
-    write (*,*) "note files have to be in the right order"
-    call abort()
-  endif
-
-  dsetname = fname_ux ( 1:index( fname_ux, '_' )-1 )
-  call fetch_attributes( fname_ux, dsetname, nx, ny, nz, xl, yl, zl, time )
+  call fetch_attributes( fname_ux, get_dsetname(fname_ux), nx, ny, nz, xl, yl, zl, time )
 
   pi = 4.d0 *datan(1.d0)
   scalex = 2.d0*pi/xl
@@ -561,33 +637,42 @@ subroutine convert_velocity()
   allocate(uk(ca(1):cb(1),ca(2):cb(2),ca(3):cb(3),1:3))
   allocate(workc(ca(1):cb(1),ca(2):cb(2),ca(3):cb(3),1:3))
 
-  ! read vorticity to u
+  ! read vorticity from files to u
   call read_single_file ( fname_ux, u(:,:,:,1) )
   call read_single_file ( fname_uy, u(:,:,:,2) )
   call read_single_file ( fname_uz, u(:,:,:,3) )
 
-  workr = u ! workr is original vorticity
+  ! copy original vorticity to workr (for comparison later, error checks)
+  workr = u
 
   call Vorticity2Velocity(uk,workc,u)
-  call ifft3 (ink=uk, outx=u)   ! u is velocity in phys space
+  call ifft3 (ink=uk, outx=u)
 
-  ! now u contains the vorticity in physical space
-  fname_ux='ux'//fname_ux(index(fname_ux,'_'):index(fname_ux,'.')-1)
-  fname_uy='uy'//fname_uy(index(fname_uy,'_'):index(fname_uy,'.')-1)
-  fname_uz='uz'//fname_uz(index(fname_uz,'_'):index(fname_uz,'.')-1)
+  ! now u contains the velocity in physical space
+  call save_field_hdf5 ( time,fname_outx,u(:,:,:,1) )
+  call save_field_hdf5 ( time,fname_outy,u(:,:,:,2) )
+  call save_field_hdf5 ( time,fname_outz,u(:,:,:,3) )
 
-  call save_field_hdf5 ( time,fname_ux,u(:,:,:,1))
-  if (mpirank==0) write(*,*) "Wrote ux to "//trim(fname_ux)
-  call save_field_hdf5 ( time,fname_uy,u(:,:,:,2))
-  if (mpirank==0) write(*,*) "Wrote uy to "//trim(fname_uy)
-  call save_field_hdf5 ( time,fname_uz,u(:,:,:,3))
-  if (mpirank==0) write(*,*) "Wrote uz to "//trim(fname_uz)
-
+  if (mpirank==0) then
+    write(*,*) "Done writing output!"
+    write(*,'(80("-"))')
+    write(*,*) "Done computing, performing some analysis of the result..."
+  endif
+  !-----------------------------------------------------------------------------
+  ! check divergence of new field
+  !-----------------------------------------------------------------------------
   call divergence(uk,workc(:,:,:,1))
   call ifft(ink=workc(:,:,:,1),outx=u(:,:,:,1))
-  !call save_field_hdf5 ( time,"divu_0000",u(:,:,:,1))
-  write(*,*) "maximum divergence=", fieldmax(u(:,:,:,1)), fieldmin(u(:,:,:,1))
+  divu_max = fieldmax(u(:,:,:,1))
+  if (mpirank==0) write(*,*) "max(div(u))", divu_max
+  divu_max = fieldmin(u(:,:,:,1))
+  if (mpirank==0) write(*,*) "min(div(u))", divu_max
 
+  deallocate(workc)
+
+  !-----------------------------------------------------------------------------
+  ! check if the curl of computed velocity is the same as input values
+  !-----------------------------------------------------------------------------
   call curl3_inplace(uk)
   call ifft3(ink=uk, outx=u)
 
@@ -596,13 +681,36 @@ subroutine convert_velocity()
   u(:,:,:,2)=u(:,:,:,2)-workr(:,:,:,2)
   u(:,:,:,3)=u(:,:,:,3)-workr(:,:,:,3)
 
-  write(*,*) "max diff vor_original-curl(result)", fieldmax(u(:,:,:,1))
-  write(*,*) "max diff vor_original-curl(result)", fieldmax(u(:,:,:,2))
-  write(*,*) "max diff vor_original-curl(result)", fieldmax(u(:,:,:,3))
+  errx = fieldmax(u(:,:,:,1))
+  erry = fieldmax(u(:,:,:,2))
+  errz = fieldmax(u(:,:,:,3))
+
+  if (mpirank==0) then
+    write(*,*) "max diff vor_original-curl(result)", errx
+    write(*,*) "max diff vor_original-curl(result)", erry
+    write(*,*) "max diff vor_original-curl(result)", errz
+  endif
+
+  ! check relative difference in mag(vor):
+  errx=0.d0
+  erry=0.d0
+  do iz=ra(3),rb(3)
+    do iy=ra(2),rb(2)
+      do ix=ra(1),rb(1)
+        errx = errx + sqrt(u(ix,iy,iz,1)**2 + u(ix,iy,iz,2)**2 + u(ix,iy,iz,3)**2)
+        erry = erry + sqrt(workr(ix,iy,iz,1)**2 + workr(ix,iy,iz,2)**2 + workr(ix,iy,iz,3)**2)
+      enddo
+    enddo
+  enddo
+
+  call MPI_ALLREDUCE (errx,errz,1,MPI_DOUBLE_PRECISION,MPI_MAX,MPI_COMM_WORLD,mpicode)
+  call MPI_ALLREDUCE (erry,divu_max,1,MPI_DOUBLE_PRECISION,MPI_MAX,MPI_COMM_WORLD,mpicode)
+  ! relative error:
+  if (mpirank==0) write(*,*) "relative error in magnitude:", errz/divu_max
 
   deallocate (u)
   deallocate (uk)
-  deallocate (workc,workr)
+  deallocate (workr)
   call fft_free()
 
 end subroutine convert_velocity
@@ -2245,9 +2353,13 @@ subroutine magnitude_post()
   call check_file_exists( fname_uy )
   call check_file_exists( fname_uz )
 
-  if (mpirank==0) write(*,*) "Computing magnitude of vector from these files: "
-  if (mpirank==0) write(*,*) trim(adjustl(fname_ux)), trim(adjustl(fname_uy)), trim(adjustl(fname_uz))
-  if (mpirank==0) write(*,*) "Outfile="//trim(adjustl(outfile))
+  if (mpirank==0) then
+    write(*,*) "Computing magnitude of vector from these files: "
+    write(*,*) trim(adjustl(fname_ux))
+    write(*,*) trim(adjustl(fname_uy))
+    write(*,*) trim(adjustl(fname_uz))
+    write(*,*) "Outfile="//trim(adjustl(outfile))
+  endif
 
   dsetname = get_dsetname( fname_ux )
   call fetch_attributes( fname_ux, dsetname, nx, ny, nz, xl, yl, zl, time )
