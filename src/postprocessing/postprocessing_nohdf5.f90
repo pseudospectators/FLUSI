b3fad5b014e3177f2438d8a99f93ddad38420a13
dkolom <dkolom@gmail.com>
2017-05-24 19:13:45 +0900

    ES3 implementation of current FLUSI version

---
+++ src/postprocessing/postprocessing_nohdf5.f90
@@ -0,0 +1,7 @@
+!-------------------------------------------------------------------------------
+! This is a stub. Postprocessing tools are not available without HDF5 support.
+!-------------------------------------------------------------------------------
+subroutine postprocessing()
+  write (*,*) 'Postprocessing without HDF5 not implemented.'
+  call abort()
+end subroutine
