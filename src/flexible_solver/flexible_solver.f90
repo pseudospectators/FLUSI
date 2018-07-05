0d922dc22891a95ac3e115d14775c2f595aaf41e
Truong-Hung <truongdinhhung90@gmail.com>
2018-07-05 17:51:24 +0200

    modify mask generation

--- src/flexible_solver/flexible_solver.f90
+++ src/flexible_solver/flexible_solver.f90
@@ -9,32 +9,31 @@ module flexible_model
   !----------------------------------------------
   ! module global variables
   !----------------------------------------------
+  integer,parameter :: nWings = 1
   integer,parameter :: nVeins = 5
   integer,parameter :: nVeins_BC = 2
   integer,parameter :: nMembranes = 1
-  integer,save :: np, debug_pressure
+  integer,save :: np
   ! see "type solid" about nsmax 06 Aug 2014
   integer,parameter :: npmax = 1000
   integer,parameter :: nvmax = 2*npmax
   integer,parameter :: nmmax = 2*npmax
   ! TODO: move these into the solid model datastructure
-  real(kind=pr),save :: t_wing, L_vein, N_smooth
-  real(kind=pr),save :: T_release
+
+  !real(kind=pr),save :: T_release
   real(kind=pr), parameter :: error_stop = 1.0e-6
-  character(len=strlen),save :: imposed_motion_leadingedge, TimeMethodSolid
-  character(len=strlen),save :: has_cylinder
-  character(len=strlen),save :: interp
-  character(len=strlen),save :: infinite, plate_shape
+  !character(len=strlen),save :: imposed_motion_leadingedge, TimeMethodSolid
+
 
   ! this is a hack to avoid allocating/deallacting these arrays in every time
   ! step. turing fails with memory issues if done otherwise. 7 Aug 2014
-  real(kind=pr),save,dimension(:,:,:,:), allocatable :: surfaces
-  real(kind=pr),save,dimension(:,:,:), allocatable :: p_surface, p_surface_local
-  real(kind=pr),save,dimension(:,:), allocatable :: active_points
-  real(kind=pr),save,dimension(:),allocatable :: heights
+  !real(kind=pr),save,dimension(:,:,:,:), allocatable :: surfaces
+  !real(kind=pr),save,dimension(:,:,:), allocatable :: p_surface, p_surface_local
+  !real(kind=pr),save,dimension(:,:), allocatable :: active_points
+  !real(kind=pr),save,dimension(:),allocatable :: heights
 
   !----------------------------------------------
-  ! Solid datatype
+  ! Wing datatype
   !----------------------------------------------
   type Wing
     ! These arrays are statically allocated (they lie thus on
@@ -45,8 +44,8 @@ module flexible_model
     real(kind=pr),dimension(1:npmax) :: x,y,z
     real(kind=pr),dimension(1:npmax) :: vx,vy,vz
     real(kind=pr),dimension(1:npmax) :: u_old, u_oldold
-    integer,dimension(1:2*npmax,4) :: tri_elements
-    real(kind=pr),dimension(ra(1):rb(1),ra(2):rb(2),ra(3):rb(3)) :: mask
+    integer,dimension(1:nmmax,4) :: tri_elements
+    
     ! Matrix contains identification of vein points connected by bending springs
     ! and their initial and current bending angles on both directions y and z:
     real(kind=pr),dimension(1:nvmax,8,nVeins) :: Veins_bending
@@ -60,7 +59,7 @@ module flexible_model
     real(kind=pr),dimension(1:nmmax,5,nMembranes) :: Membranes_extension
     real(kind=pr),dimension(1:nmmax,5) :: Membrane_edge
     ! external forces:
-    real(kind=pr),dimension(0:nsmax) :: pressure_old, pressure_new
+    !real(kind=pr),dimension(1:npmax) :: forces_ext
     ! material properties:
     real(kind=pr),dimension(1:nVeins) :: EIy, EIz, kby0, kbz0
     real(kind=pr),dimension(1:nVeins_BC) :: EIy_BC, EIz_BC, kby0_BC, kbz0_BC
@@ -79,7 +78,7 @@ module flexible_model
     real(kind=pr),dimension(1:nvmax) :: m_me
 
     ! grid and width in rigid direction:
-    real(kind=pr),dimension(1:nVeins+nBCs) :: L_veins
+    real(kind=pr) :: t_wing, wing_smoothing
     ! values of theta and theta_dot at times t^(n) and t^(n-1)
     !real(kind=pr),dimension(0:nsmax) :: theta_old, theta_oldold
     !real(kind=pr),dimension(0:nsmax) :: theta_dot_old, theta_dot_oldold
@@ -87,7 +86,7 @@ module flexible_model
     ! real(kind=pr),dimension(0:nsmax,1:6) :: wing_oldold
     !real(kind=pr),dimension(1:2) :: Force, Force_unst, Force_press, Inertial_Force
     !real(kind=pr) :: E_kinetic, E_elastic
-    real(kind=pr) :: x0, y0, z0, Anglewing_y, Anglewing_z, phase
+    real(kind=pr) :: x0, y0, z0, Anglewing_y, Anglewing_z
     ! we need the previous time step for the BDF solver
     real(kind=pr) :: dt_old
     ! these replace the save variables in the unst correction computation:
@@ -106,6 +105,7 @@ module flexible_model
 ! include "wingForces.f90"
 ! include "prescribed_wing.f90"
  include "flexible_solver_wrapper.f90"
+ include "flexible_tri_mask.f90"
 
 
 
